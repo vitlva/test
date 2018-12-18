8b5f0f8e5c1b140bcbdbe51514070c664bfb07e1
vit <vitlva2015@yandex.ru>
2018-12-18 23:16:25 +0600

    change Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -1,7 +1,19 @@
 using System;
 
-class Program {
-	static void Main() {
-		Console.WriteLine("Hello world!!!");
+namespace persons {
+	class Program {
+		static void Main() {			
+			Console.WriteLine("Hello world!!!");
+			Person p = new Person(1,"Peter");
+			Person p1 = new Person(1,"Peter");
+			Console.WriteLine("Hello " + p);
+			Console.WriteLine("Hello " + p1);
+			Console.WriteLine("p == p1 {0} ",p == p1);
+			Console.WriteLine("p != p1 {0} ",p != p1);
+			//Console.WriteLine("xx = "+Math.Sqrt(1225));
+			//Console.WriteLine("xx = "+(45*45));
+			Console.WriteLine("p.Equals(p1) {0} ",p.Equals(p1));						
+			Console.WriteLine("!p.Equals(p1) {0} ",!p.Equals(p1));						
+		}
 	}
 }
